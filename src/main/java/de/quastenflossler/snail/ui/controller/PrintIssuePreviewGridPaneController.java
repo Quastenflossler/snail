a183afe306ce72bc6c616de9b3ec662d9a333630
Basti <bastinator@online.de>
2020-05-15 19:32:36 +0200

    Issue print preview finished.

--- src/main/java/de/quastenflossler/snail/ui/controller/PrintIssuePreviewGridPaneController.java
+++ src/main/java/de/quastenflossler/snail/ui/controller/PrintIssuePreviewGridPaneController.java
@@ -9,7 +9,6 @@ import javafx.scene.image.ImageView;
 import javax.annotation.Resource;
 import javax.inject.Named;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 @Named(value = PrintIssuePreviewGridPaneController.RESOURCE_NAME)
@@ -23,14 +22,51 @@ public class PrintIssuePreviewGridPaneController {
     @FXML
     public ImageView previewIssueLogo;
 
+    @FXML
+    public Label previewIssueKeyLabel;
+
+    @FXML
+    public Label previewEpicKeyLabel;
+
+    @FXML
+    public Label previewEpicNameLabel;
+
+    @FXML
+    public Label previewStakeholderLabel;
+
+    @FXML
+    public Label previewPlannedSprintLabel;
+
+    @FXML
+    public Label previewDeadlineLabel;
+
+    @FXML
+    public Label previewDescriptionLabel;
+
+    @FXML
+    public Label previewStoryPointsLabel;
+
+    @FXML
+    public Label previewAcceptanceCriteriaLabel;
+
     @Resource(name = PrintIssueModel.RESOURCE_NAME)
     private PrintIssueModel printIssueModel;
 
-    public void initialize() throws URISyntaxException, IOException {
+    public void initialize() throws IOException {
 
         previewIssueTitleLabel.textProperty().bindBidirectional(printIssueModel.issueTitleProperty());
 
         URL ingoFilePath = getClass().getResource("/assets/ingo.png");
         previewIssueLogo.setImage(new Image(ingoFilePath.openStream()));
+
+        previewIssueKeyLabel.textProperty().bindBidirectional(printIssueModel.issueKeyProperty());
+        previewEpicKeyLabel.textProperty().bindBidirectional(printIssueModel.epicKeyProperty());
+        previewEpicNameLabel.textProperty().bindBidirectional(printIssueModel.epicNameProperty());
+        previewStakeholderLabel.textProperty().bindBidirectional(printIssueModel.stakeholderProperty());
+        previewPlannedSprintLabel.textProperty().bindBidirectional(printIssueModel.plannedSprintProperty());
+        previewDeadlineLabel.textProperty().bindBidirectional(printIssueModel.deadlineProperty());
+        previewDescriptionLabel.textProperty().bindBidirectional(printIssueModel.issueDescriptionProperty());
+        previewStoryPointsLabel.textProperty().bindBidirectional(printIssueModel.storyPointsProperty());
+        previewAcceptanceCriteriaLabel.textProperty().bindBidirectional(printIssueModel.acceptanceCriteriaProperty());
     }
 }
